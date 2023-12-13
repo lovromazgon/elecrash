e3482ab75c81f02dd76bb34925b0ec5228ebc065
Lovro Mažgon <lovro.mazgon@gmail.com>
2023-12-13 08:19:29 -0500

    flags

--- cmd/elecrash/main.go
+++ cmd/elecrash/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	ui "github.com/gizak/termui/v3"
@@ -8,12 +9,18 @@ import (
 )
 
 func main() {
+	var (
+		elevators = flag.Int("e", 4, "number of elevators")
+		floors    = flag.Int("f", 8, "number of floors")
+	)
+	flag.Parse()
+
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
 	defer ui.Close()
 
-	b := elecrash.NewBackground(5, 4)
+	b := elecrash.NewBackground(*elevators, *floors)
 	ui.Render(b)
 
 	for e := range ui.PollEvents() {
